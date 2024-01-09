937f9c0f43b5cf593d648e6dac6523e879d8e842
JesusBalam <jesus.balambatun@gmail.com>
2024-01-09 13:49:19 -0500

    exit from dashboard when user token not exist

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+
 import "./page.module.css";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Dashboard() {
+  const router = useRouter();
+  useEffect(() => {
+    const existToken = localStorage.getItem("userToken");
+    if (existToken) {
+      return;
+    }
+    router.push("/login");
+  }, []);
+
   return (
     <h1
       className="dashboard-container"
